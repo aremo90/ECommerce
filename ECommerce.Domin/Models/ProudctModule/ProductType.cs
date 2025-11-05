7b5870ac41d8f8b695f2e6bf7b9d6fc72df42a66
aremo90 <mrhamza2005@gmail.com>
2025-11-05 18:32:05 +0200

    Product Table Complete

---
+++ ECommerce.Domin/Models/ProudctModule/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domin.Models.ProudctModule
+{
+    public class ProductType : BaseEntity<int>
+    {
+       
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ ECommerce.Persistence/Data/Configurations/ProductConfig/ProductTypeConfig.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Domin.Models.ProudctModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Data.Configurations.ProductConfig
+{
+    public class ProductTypeConfig : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(X => X.Name)
+                .HasMaxLength(100);
+        }
+    }
+}
